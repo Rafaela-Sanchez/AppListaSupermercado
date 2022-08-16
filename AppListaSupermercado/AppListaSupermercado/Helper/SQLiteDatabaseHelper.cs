bc901905fc410e6ebfb2ab49a47e004522268c4d
Rafaela Sanchez <rafaelasanchezjahu@hotmail.com>
2022-08-16 12:19:05 -0300

    Criação de Pastas do Projeto, inserção de métodos na classe SQLiteDatabaseHelper

---
+++ AppListaSupermercado/AppListaSupermercado/Helper/SQLiteDatabaseHelper.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using AppListaSupermercado.Model;
+
+
+namespace AppListaSupermercado.Helper
+{
+    public class SQLiteDatabaseHelper
+    {
+        /** Arquivo somente leitura, que abre uma conexão assíncrona, sem interferir no fluxo
+         * principal do aplicativo
+         */
+
+        readonly SQLite.SQLiteAsyncConnection con;
+
+        /** método construtor que abre uma conexão com o banco de forma assíncrona.
+         * Path = caminho -> onde o arquivo se encontra
+         */
+
+        public SQLiteDatabaseHelper (string path)
+        {
+            con = new SQLite.SQLiteAsyncConnection (path);
+
+            /** criando uma tabela de forma assíncrona, baseada no model produto
+             */
+
+            con.CreateTableAsync<Produto>().Wait();
+
+        }
+
+
+        /**
+         * O método insert espera receber um modelo preenchido 
+         * para que ele seja garavdo no banco de dados :)
+         * Pega o que o usuário digitou na interface gráfica e preenche os campos da classe.
+         * Uma vez que preenchido, pode ser transportado de lugar (I.G para Helper- gravar no sqlite)
+         */
+        public void insert(Produto p)
+        {
+
+        }
+
+        public void update(Produto p)
+        {
+
+        }
+
+        public Task<Produto> GetById(int id)
+        {
+            return new Produto();
+        }
+
+        public Task<List<Produto>> getAll()
+        {
+
+        }
+
+        public void delete(int id)
+        {
+
+        }
+    }
+}
---
+++ AppListaSupermercado/AppListaSupermercado/Model/Produto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace AppListaSupermercado.Model
+{
+    public class Produto
+    {
+        [PrimaryKey, AutoIncrement]
+        public string Id { get; set; }
+        public string Descricao { get; set; }
+
+        public double Preco { get; set; }
+
+        public double Quantidade { get; set; }
+
+    }
+}
